34938cde41da621ac2f4ccda2b632a1a4e2f7511
El Diablo <estesquefawer@gmail.com>
2020-10-20 10:28:43 -0500

    "creted server.js

--- server.js
+++ server.js
@@ -1,25 +1,28 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 require('dotenv').config();
 
 const app = express();
+const port = process.env.PORT || 5000
 
 // cors Middleware
 app.use(cors());
 app.use(express.json());
 
 // DB Config
-const db = require('./config/keys').mongoURI;
+// const db = require('./config/keys')
 
 // Connect to MongoDB
-mongoose
-.connect(db)
-.then(() => console.log('MongoDB is Connected...'))
-.catch(err => console.log(err));
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established succesfully");
+})
 
-
-const port = process.env.PORT || 5000
-
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
